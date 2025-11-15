8eb9de056eead02c43df2d085ec6070e3db9354f
DASAYEV GOMES NUNES <dasayevgn@gmail.com>
2025-11-15 11:15:59 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,15 @@
+<html>
+
+    <h1 class="ajuste3">Login</h1>
+
+    <form class="ajuste3" action="passagens.html">
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required><br><br>
+        <label for="pwd">Senha:</label>
+        <input type="password" id="pwd" name="pwd" required><br><br>
+        <input type="submit" value="Entrar">
+    </form>
+
+    
+
+</html>
