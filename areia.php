f9a01f912f66d213c7a6ba176c0d9647071e9e65
Juliodev07 <juliothomass16@gmail.com>
2025-11-16 17:46:18 -0300

    cidades

---
+++ areia.php
@@ -0,0 +1,101 @@
+<body>
+  <header class="header">
+    <a href="passagens.php"> <img src="https://bing.com/th/id/BCO.b912d4d6-68a8-4249-8b7e-364e6d88df85.png"></a>
+
+    <ul>
+      <li><a href="cadastro.php"><i class="bi bi-person-circle"></i>Cadastro</a></li>
+      <li><a href="login.php"><i class="bi bi-door-open-fill"></i>Login</a></li>
+
+    </ul>
+  </header>
+  <nav class="nav-bar">
+    <ul>
+      <li><a href="passagens.php"><i class="bi bi-bus-front-fill"></i>Passagens</a></li>
+      <li><a href="carros.php"><i class="bi bi-car-front-fill"></i>Carros</a></li>
+      <li><a href="hoteis.php"><i class="bi bi-building-fill"></i>Hotéis</a></li>
+      <li><a href="promocoes.php"><i class="bi bi-megaphone-fill"></i>Promoções</a></li>
+
+    </ul>
+  </nav>
+  <h1>Cidade de Areia</h1>
+  <img class="moldura"
+    src="https://i0.wp.com/heroncid.maispb.com.br/wp-content/uploads/2017/07/Areia_1.jpg?resize=800%2C344&quality=90&strip=all&ssl=1"
+    alt="Cidade de Areia" width="500" heigth="600" />
+  <p style="text-align: justify;">
+        <form>
+  <div>
+    <input type="radio" id="somente ida" name="trip" value="somente ida" checked>
+    <label for="somente ida">Somente Ida</label>
+    <input type="radio" id="ida e volta" name="trip" value="ida e volta">
+    <label for="roundtrip">Ida e Volta</label>
+  </div>
+  <div>
+    <label for="saida">Ida</label>
+    <input type="text" id="saida" name="ida" placeholder="__/__/__">
+    <label for="volta">Volta</label>
+    <input type="text" id="volta" name="volta" placeholder="__/__/__">
+  </div>
+  <button type="submit">Buscar</button>
+</form>
+    <b>AREIA</b> Dos 223 municípios paraibanos, a maioria ainda não conseguiu encontrar o fio da meada. Me refiro a um
+    dado: falta a um imenso contingente
+    encontrar sua vocação e potencialidade, condições basilares para o traçado felizmente no rol destas exceções. Por
+    aqui, é fácil constatar um DNA, uma
+    identidade bem definida, de sua essência e aptidão. Encravada no alto da Serra, a terra de Pedro Américo – um dos
+    maiores pintores romancistas do
+    mundo – sabe ser e sabe dar. A valorização de seu significado histórico é respirado e vivido, com prazer, pelos seus
+    24 mil habitantes. Os museus e a
+    manutenção da sua arquitetura colonial e o afeto pela perpetuação dos costumes antigos, como a exploração da cana de
+    açúcar, dão uma atmosfera
+    diferenciada.
+  </p>
+
+    <footer>
+    <h1><i>ParaíbaViagens</i></h1>
+    <div class="footer-columns">
+
+      <div class="footer-column">
+        <h2>Institucional</h2>
+        <ul>
+          <li><a href="#">Sobre Nós</a></li>
+          <li><a href="#">Sala de Imprensa</a></li>
+          <li><a href="#">Trabalhe Conosco - Matriz</a></li>
+          <li><a href="#">Trabalhe Conosco - Lojas</a></li>
+          <li><a href="#">Seja um Franqueado</a></li>
+        </ul>
+      </div>
+
+      <div class="footer-column">
+        <h2>Atendimento</h2>
+        <ul>
+          <li><a href="#">Central de Atendimento</a></li>
+          <li><a href="#">Suporte Pós Venda</a></li>
+          <li>11 3003 9282 • Opção 2 | 1 URA</li>
+          <li>Segunda à Sábado: 09h às 21h</li>
+        </ul>
+      </div>
+
+      <div class="footer-column">
+        <h2>Relacionamento</h2>
+        <ul>
+          <li><a href="#">Central de Relacionamento ao Cliente</a></li>
+          <li>11 3003-9282 • Opção 2 | 2 URA</li>
+          <li>Segunda à Sexta: 09h às 20h</li>
+          <li>Sábado e Feriados: 09h às 16h</li>
+          <li><a href="#">Central de Ajuda</a></li>
+        </ul>
+      </div>
+
+      <div class="footer-column dropdown">
+        <h2>Mais Informações</h2>
+        <button>Outros Links ▼</button>
+        <div class="dropdown-content">
+          <a href="#">Política de Privacidade</a>
+          <a href="#">Termos de Uso</a>
+          <a href="#">Mapa do Site</a>
+        </div>
+      </div>
+
+    </div>
+  </footer>
+</body>
---
+++ jp.php
@@ -0,0 +1,112 @@
+<body>
+    <header class="header">
+        <a href="passagens.php"> <img src="https://bing.com/th/id/BCO.b912d4d6-68a8-4249-8b7e-364e6d88df85.png"></a>
+
+        <ul>
+            <li><a href="cadastro.php"><i class="bi bi-person-circle"></i>Cadastro</a></li>
+            <li><a href="login.php"><i class="bi bi-door-open-fill"></i>Login</a></li>
+
+        </ul>
+    </header>
+    <nav class="nav-bar">
+        <ul>
+            <li><a href="passagens.php"><i class="bi bi-bus-front-fill"></i>Passagens</a></li>
+            <li><a href="carros.php"><i class="bi bi-car-front-fill"></i>Carros</a></li>
+            <li><a href="hoteis.php"><i class="bi bi-building-fill"></i>Hotéis</a></li>
+            <li><a href="promocoes.php"><i class="bi bi-megaphone-fill"></i>Promoções</a></li>
+        </ul>
+    </nav>
+    <h1> João Pessoa</h1>
+    <img class="moldura" src="https://bhecoturismo.com.br/wp-content/uploads/2025/03/Capa-Joao-Pessoa.jpg"  alt="Cidade de Areia"
+        width="500" heigth="600" />
+        <form>
+  <div>
+    <input type="radio" id="somente ida" name="trip" value="somente ida" checked>
+    <label for="somente ida">Somente Ida</label>
+    <input type="radio" id="ida e volta" name="trip" value="ida e volta">
+    <label for="roundtrip">Ida e Volta</label>
+  </div>
+  <div>
+    <label for="saida">Ida</label>
+    <input type="text" id="saida" name="ida" placeholder="__/__/__">
+    <label for="volta">Volta</label>
+    <input type="text" id="volta" name="volta" placeholder="__/__/__">
+  </div>
+  <button type="submit">Buscar</button>
+</form>
+    <p style="text-align: justify;">
+        <b>A cidade de João Pessoa</b> é um município brasileiro, capital do estado da Paraíba. Com população de mais de
+        830 mil habitantes no censo de 2022, a capital paraibana é o município mais populoso de seu estado, o sétimo da
+        Região Nordeste e o 20° do Brasil. A Região Metropolitana de João Pessoa, formada pela capital e mais onze
+        municípios, tinha uma população de aproximadamente 1,4 milhão de pessoas.
+        Fundada em 1585 com o nome de Cidade Real de Nossa Senhora das Neves, logo passou a se chamar de Filipeia de
+        Nossa Senhora das Neves em 1588 em homenagem ao rei Filipe II que, na época, acumulava as coroas da Espanha e de
+        Portugal. Posteriormente chamada Frederikstad, foi uma das duas principais cidades da Nova Holanda, junto com
+        Mauritsstadt (a atual Recife), na segunda metade do século XVII. Possui antigo e vasto patrimônio histórico,
+        similar ao de Olinda. Somente em 1930 tomou sua denominação atual.
+        Uma das capitais de melhor qualidade de vida do Nordeste, é conhecida como "Porta do Sol", devido ao fato de, no
+        município, estar localizada a Ponta do Seixas, que é o ponto mais oriental da América, o que faz a cidade ser
+        conhecida como o lugar "onde o sol nasce primeiro no continente americano". Atualmente, é a 55.ª cidade mais
+        rica do país em PIB nominal e a 9.ª mais rica do Nordeste, com um PIB de cerca de 22 bilhões de reais. Conta com
+        Jardim Botânico Benjamim Maranhão, localizado na área central da cidade com 515 hectares de mata atlântica
+        preservada, constituindo a maior floresta semiequatorial nativa plana densamente cercada por área urbana do
+        mundo.
+        A cidade teve o seu centro histórico tombado pelo Instituto do Patrimônio Histórico e Artístico Nacional (IPHAN)
+        em 2007, e inscrito nos seguintes Livros do Tombo: Histórico e Arqueológico, Etnográfico e Paisagístico. Seu
+        tombamento foi em circunstância de ser uma das primeiras cidades fundadas no Brasil. Recebeu em 2017 o título de
+        cidade criativa pela Unesco, colocando João Pessoa como "Cidade Brasileira do Artesanato". O reconhecimento de
+        João Pessoa a coloca na rota turística brasileira por sua arte popular. Esse reconhecimento teve grande
+        contribuição e influência, devido ao projeto sereias da Penha; onde mulheres artesãs realizam o trabalho manual,
+        dialogando com o design, moda, economia criativa, destacando a arte com escamas de peixe em fios de cobre.
+
+  <footer>
+    <h1><i>ParaíbaViagens</i></h1>
+    <div class="footer-columns">
+
+      <div class="footer-column">
+        <h2>Institucional</h2>
+        <ul>
+          <li><a href="#">Sobre Nós</a></li>
+          <li><a href="#">Sala de Imprensa</a></li>
+          <li><a href="#">Trabalhe Conosco - Matriz</a></li>
+          <li><a href="#">Trabalhe Conosco - Lojas</a></li>
+          <li><a href="#">Seja um Franqueado</a></li>
+        </ul>
+      </div>
+
+      <div class="footer-column">
+        <h2>Atendimento</h2>
+        <ul>
+          <li><a href="#">Central de Atendimento</a></li>
+          <li><a href="#">Suporte Pós Venda</a></li>
+          <li>11 3003 9282 • Opção 2 | 1 URA</li>
+          <li>Segunda à Sábado: 09h às 21h</li>
+        </ul>
+      </div>
+
+      <div class="footer-column">
+        <h2>Relacionamento</h2>
+        <ul>
+          <li><a href="#">Central de Relacionamento ao Cliente</a></li>
+          <li>11 3003-9282 • Opção 2 | 2 URA</li>
+          <li>Segunda à Sexta: 09h às 20h</li>
+          <li>Sábado e Feriados: 09h às 16h</li>
+          <li><a href="#">Central de Ajuda</a></li>
+        </ul>
+      </div>
+
+      <div class="footer-column dropdown">
+        <h2>Mais Informações</h2>
+        <button>Outros Links ▼</button>
+        <div class="dropdown-content">
+          <a href="#">Política de Privacidade</a>
+          <a href="#">Termos de Uso</a>
+          <a href="#">Mapa do Site</a>
+        </div>
+      </div>
+
+    </div>
+  </footer>
+
+
+</body>
