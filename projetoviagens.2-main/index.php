e4e013e34000b7a561b6e03498a3e9e984d4b913
Moisés Fênix <moisesfenix14@gmail.com>
2025-11-19 17:27:34 -0300

    Add files via upload

---
+++ projetoviagens.2-main/index.php
@@ -0,0 +1,21 @@
+<?php
+
+    include_once("topo.php");
+    include_once("menu.php");
+
+    if(empty($_SERVER["QUERY_STRING"])){
+        $pg = "conteudo";
+        include_once("$pg.php");
+    }elseif($_GET['pg']){
+        $pg = $_GET['pg'];
+        include_once "$pg.php";
+    }else{
+        echo "Página não encontrada";
+
+    }
+
+
+    include_once("rodape.php");
+
+
+?>
---
+++ projetoviagens.2-main/promocoes.php
@@ -0,0 +1,16 @@
+ <link rel="stylesheet" href="promocoes.css">
+ </div>
+
+  <div class="carrossel">
+    <div class="slides">
+      <div class="slide"><a href="passagens.html"><img
+            src="imagens\voos por toda paraiba.png"></a>
+      </div>
+      <div class="slide"><a href="passagens.html"><img
+            src="imagens\promocionalhotel.png"></a>
+      </div>
+      <div class="slide"><a href="passagens.html"><img
+            src="imagens\promocionalcarros.png"></a>
+      </div>
+      
+  </div>
