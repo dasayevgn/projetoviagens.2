95b73de3c1385f721d433b9635af8f6fd00a9c1b
DASAYEV GOMES NUNES <dasayevgn@gmail.com>
2025-11-26 19:48:51 -0300

    Add files via upload

--- cadastro.php
+++ cadastro.php
@@ -1,23 +1,25 @@
-<html>
+
 
     <h1 class="ajuste3">Cadastro</h1>
 
-    <form class="ajuste3" action="passagens.html">
-        <label>Nome:</label>
-        <input type="text" name="nome" required><br><br>
-        <label>Idade:</label>
-        <input type="number" min="18" required><br><br>
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required><br><br>
-        <label for="pwd">Senha:</label>
-        <input type="password" id="pwd" name="pwd" required><br><br>
-        <label for="pwd">Confirme a senha:</label>
-        <input type="password" id="pwd" name="pwd" minlength="8" required><br><br>
-        <input type="checkbox" name="termo" required> <!--required torna o preenchimento obrigatorio-->
-        <label>Concordo com os termos de privacidade do Site</label><br><br>
-        <input type="submit" value="Entrar">
-    </form>
+    <form class="ajuste3" action="?pg=clientes-cadastro" method="post">
+    <label>Nome:</label>
+    <input type="text" name="nome" required><br><br>
 
-    
+    <label>Idade:</label>
+    <input type="number" name="idade" min="18" required><br><br>
 
-</html>
+    <label>Email:</label>
+    <input type="email" name="email" required><br><br>
+
+    <label>Senha:</label>
+    <input type="password" name="senha" minlength="8" required><br><br>
+
+    <label>
+        <input type="checkbox" name="termo" required>
+        Aceito os termos
+    </label><br><br>
+
+    <input type="submit" value="Cadastrar Cliente">
+
+</form>
